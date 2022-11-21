38963f5fc0a238de46323b9ee07f2d2dae81f179
GaeulLee <rhakq23@gmail.com>
2022-11-21 10:11:23 +0900

    vo 생성

---
+++ src/main/java/vo/Cash.java
@@ -0,0 +1,48 @@
+package vo;
+
+public class Cash {
+	private int cashNo;
+ // private Category category; -> 원래는 이렇게 했다면(cash 타입) 이번엔 아래와 같은 방식으로
+	private int categoryNo; // FK(왜래키)를 -> INNER JOIN 하여 -> Map 타입 으로
+	private long cashPrice; // 큰 숫자가 들어올 수 있기 때문에 long 타입으로
+	private String cashMemo;
+	private String updatedate;
+	private String createdate;
+	
+	public int getCashNo() {
+		return cashNo;
+	}
+	public void setCashNo(int cashNo) {
+		this.cashNo = cashNo;
+	}
+	public int getCategoryNo() {
+		return categoryNo;
+	}
+	public void setCategoryNo(int categoryNo) {
+		this.categoryNo = categoryNo;
+	}
+	public long getCashPrice() {
+		return cashPrice;
+	}
+	public void setCashPrice(long cashPrice) {
+		this.cashPrice = cashPrice;
+	}
+	public String getCashMemo() {
+		return cashMemo;
+	}
+	public void setCashMemo(String cashMemo) {
+		this.cashMemo = cashMemo;
+	}
+	public String getUpdatedate() {
+		return updatedate;
+	}
+	public void setUpdatedate(String updatedate) {
+		this.updatedate = updatedate;
+	}
+	public String getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}	
+}
